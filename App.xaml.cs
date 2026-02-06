985b74c2acf4fcb1bf05cd280bd6512c8802ceee
Avronil Htech <58687761+AvronilHtech@users.noreply.github.com>
2026-02-06 12:48:02 +0530

    Create MAUI routine app with 10-minute reminders

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+using RoutineHelper.Views;
+
+namespace RoutineHelper;
+
+public partial class App : Application
+{
+    public App(MainPage mainPage)
+    {
+        InitializeComponent();
+
+        MainPage = new NavigationPage(mainPage);
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+namespace RoutineHelper;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
